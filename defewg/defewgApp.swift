e94585ad572a7ce8086601a865dba01d51fe871c
amg-03 <91329661+amg-03@users.noreply.github.com>
2022-09-23 16:32:50 +0300

    Initial Commit

---
+++ defewg/defewgApp.swift
@@ -0,0 +1,17 @@
+//
+//  defewgApp.swift
+//  defewg
+//
+//  Created by Арсений Гулевич  on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct defewgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
